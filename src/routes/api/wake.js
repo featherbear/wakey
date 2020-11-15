9b059dcde5306d618dec8d9e023d5a36595a9743
Andrew Wong <andrew.j.wong@outlook.com>
2020-11-16 01:57:58 +1100

    Wake

---
+++ src/routes/api/wake.js
@@ -0,0 +1,19 @@
+export async function post (req, res, n) {
+  const { id, wait } = req.body
+
+  if (typeof id === 'undefined') {
+    return res.finish('`id` not supplied!', 400)
+  }
+
+  let device = req.app.find()
+  if (!device) {
+    return res.finish(`Device with id \`${id}\` not found`, 404)
+  }
+
+  if (wait) {
+    return res.end(await device.wake())
+  } else {
+    device.wake()
+    return res.finish('OK')
+  }
+}
